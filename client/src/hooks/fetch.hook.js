215739ab115e5f59591a95baf0df66c1a67a2fa9
Success-akinyemi <brownsamuel915@gmail.com>
2023-09-07 09:28:39 -0700

    backend-poll

--- client/src/hooks/fetch.hook.js
+++ client/src/hooks/fetch.hook.js
@@ -3,8 +3,8 @@ import { useCallback, useEffect, useState } from "react";
 import { getEmail } from '../helpers/helper'
 
 //axios.defaults.baseURL = 'http://localhost:9000'
-axios.defaults.baseURL = 'https://bravesub-api.onrender.com'
-
+//axios.defaults.baseURL = 'https://bravesub-api.onrender.com'
+axios.defaults.baseURL= 'https://rich-gold-trout-sock.cyclic.cloud'
 
 /**Custom Hooks */
 export function useFetch(query){
